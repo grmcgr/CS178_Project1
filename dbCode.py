63e6bfa661479b5f2ffd44bac7fb55fd9566871e
grmcgr <grace.mcgrane@drake.edu>
2025-04-17 17:50:26 -0500

    final commit

--- dbCode.py
+++ dbCode.py
@@ -1,48 +1,125 @@
 # check the slides for information to add to this - from labs 
 
-
-# idea - list of contacts with most recent change with user who changed it 
-# work contacts with username / email / phone number / first name / preferred name / 
-# option to open text boxes to change the names 
-# then do a sql table of who has updated and when 
-# return me a list of all the people who username = mcgrane1 updated 
-# return me a list of who has been updated in the last week
-# user not in system - set up your username
+# dbCode.py
 
 import pymysql
-import pymysql.cursors
-import cred
-import boto3
+import cred as creds
 
-# Section 1: My SQL (RDS) Connection Helpers
-
-def get_conn():
-    """
-    Establish and return a connection to the MySQL RDS database using 
-    credentials from cred.py
-    Use DictCursor to return query results as dictionaries.
-    """
-    return pymysql.connect(
-        host=cred.host,
-        user=cred.user,
-        password=cred.password,
-        db=cred.db,
-        cursorclass=pymysql.cursors.DictCursor
+def get_all_movies():
+    conn = pymysql.connect(
+        host=creds.host,
+        user=creds.user,
+        password=creds.password,
+        db=creds.db,
+        #cursorclass=pymysql.cursors.DictCursor
     )
+    return conn
 
 def execute_query(query, args=()):
+    cur = get_all_movies().cursor(pymysql.cursors.DictCursor)
+    cur.execute(query, args)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+def get_movie_genres():
+    query = "SELECT genre_name " \
+            "FROM genre " \
+            "ORDER BY genre_name;"
+    return execute_query(query)
+
+def show_me_movies():
+    query = "SELECT m.title, " \
+        " GROUP_CONCAT(g.genre_name ORDER BY g.genre_name SEPARATOR ', ') AS genre, " \
+        " CONCAT('$', FORMAT(m.budget, 0)) AS budget, " \
+        " CONCAT('$', FORMAT(m.revenue, 0)) AS revenue, " \
+        " ROUND(m.popularity,0) AS popularity, " \
+        " m.overview " \
+        " FROM movie m " \
+        " JOIN movie_genres mg " \
+        " ON m.movie_id = mg.movie_id " \
+        " JOIN genre g " \
+        " ON mg.genre_id = g.genre_id " \
+        " GROUP BY m.movie_id, m.title, m.budget, " \
+        " m.revenue, m.popularity, m.overview " \
+        " ORDER BY m.popularity DESC;" 
+    return execute_query(query)
+
+
+import boto3
+
+# Set up DynamoDB connection (assuming same region and table name)
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('Users')  
+
+def get_all_users():
+    response = table.scan()
+    items = response.get('Items', [])
+    
+    # Convert items to a list of [username, DOB] for template rendering
+    users_list = []
+    for item in items:
+        username = item.get('Username', 'N/A')
+        dob = item.get('DOB', 'N/A')
+        users_list.append([username, dob])
+    
+    return users_list
+
+def get_user(username):
+    response = table.get_item(Key={'Username': username})
+    return response.get('Item')
+
+def user_exists(username):
+    response = table.get_item(Key={'Username': username})
+    return 'Item' in response
+
+def add_user_to_dynamo(username, dob):
+    table.put_item(
+        Item={
+            'Username': username,
+            'DOB': dob
+        }
+    )
+
+def delete_user_from_dynamo(username):
+    table.delete_item(Key={'Username': username})
+
+def update_user_dob(username, dob):
+    table.update_item(
+        Key={'Username': username},
+        UpdateExpression='SET DOB = :d',
+        ExpressionAttributeValues={':d': dob}
+    )
+
+def get_all_genres():
+    query = "SELECT genre_id, genre_name" \
+            " FROM genre " \
+            " ORDER BY genre_name;"
+    return execute_query(query)
+
+def get_top_movies_by_genre(genre_id):
+    query = """
+    SELECT 
+        m.title, 
+        g.genre_name, 
+        CONCAT('$', FORMAT(m.budget, 0)) AS budget, 
+        CONCAT('$', FORMAT(m.revenue, 0)) AS revenue, 
+        ROUND(m.popularity,0) AS popularity, 
+        m.overview 
+    FROM movie m
+    JOIN movie_genres mg ON m.movie_id = mg.movie_id
+    JOIN genre g ON mg.genre_id = g.genre_id
+    WHERE g.genre_id = %s
+    ORDER BY m.popularity DESC
+    LIMIT 25;
     """
-    Execute a SQL query using a connection to RDS. 
-    Closes the connection automatically after running the query.
-    Returns the results as a list of dictionaries.
-    """
-    conn = get_conn()
-    try:
-        with conn.cursor() as cur:
-            cur.execute(query, args)
-            rows = cur.fetchall()
-        return rows
-    finally:
-        conn.close()
+    return execute_query(query, (genre_id,))
+
+
+
+
+if __name__ == "__main__":
+    get_all_movies()
+
 
 
---
+++ flaskapp.py
@@ -0,0 +1,134 @@
+
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+import cred
+from dbCode import * # import helper functions from dbCode.py
+import pymysql
+import dbCode
+
+
+app = Flask(__name__)
+app.secret_key = 'grace-mcgrane'
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        username = request.form['Username']
+        dob = request.form['DOB']
+
+        if dbCode.user_exists(username):
+            user = dbCode.get_user(username)
+            if user.get('DOB') == dob:
+                session['user'] = username
+                flash(f'Welcome back, {username}!', 'success')
+                return redirect(url_for('movies'))  # GO TO MOVIE PAGE
+
+            else:
+                flash('DOB does not match our records.', 'danger')
+                return redirect(url_for('home'))
+
+        else:
+            flash('User not found. Please create an account.', 'warning')
+            return redirect(url_for('add_user'))     
+    return render_template('home.html') 
+
+@app.route('/add-user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        # Extract form data
+        username = request.form['Username']
+        dob = request.form['DOB']
+        
+        # Check for duplicate username
+        if dbCode.user_exists(username):
+            flash('Username already taken. Please choose another one.', 'danger')
+            return redirect(url_for('add_user'))
+        
+        # Process the data (e.g., add it to a database)
+        dbCode.add_user_to_dynamo(username, dob)
+        
+        flash('User added successfully!', 'success')  # 'success' is a category; makes a green banner at the top
+        # Redirect to home page or another page upon successful submission
+        return redirect(url_for('home'))
+    else:
+        # Render the form page if the request method is GET
+        return render_template('add_user.html')
+
+@app.route('/delete-user', methods=['GET', 'POST'])
+def delete_user():
+    if request.method == 'POST':
+        # Extract form data
+        username = request.form['Username']
+        
+        # Process the data (e.g., delete it to a database)
+        dbCode.delete_user_from_dynamo(username)
+        # For now, let's just print it to the console
+        print("User to delete:", username)
+        
+        flash('User deleted successfully!', 'warning')  # 'success' is a category; makes a green banner at the top
+        # Redirect to home page or another page upon successful submission
+        return redirect(url_for('home'))
+    else:
+        # Render the form page if the request method is GET
+        return render_template('delete_user.html')
+
+@app.route('/update-user', methods=['GET', 'POST'])
+def update_user():
+    if request.method == 'POST':
+        username = request.form['Username']
+        new_dob = request.form['DOB']
+
+        if dbCode.user_exists(username):
+            dbCode.update_user_dob(username, new_dob)
+            flash(f'DOB updated for {username}', 'success')
+        else:
+            flash(f'User {username} not found.', 'danger')
+
+        return redirect(url_for('update_user'))
+    else:
+        return render_template('update_user.html')
+
+
+@app.route('/display-users')
+def display_users():
+    # hard code a value to the users_list;
+    users_list = dbCode.get_all_users()
+    # note that this could have been a result from an SQL query :) 
+    return render_template('display_users.html', users=users_list)
+
+
+@app.route('/movies')
+def movies():
+    data = dbCode.get_movie_genres()
+    return render_template('movies.html', results=data)
+
+# make special queries and list them 
+# make an html for each of them
+@app.route('/movies/all-movies')
+def show_all_movies():
+    data = dbCode.show_me_movies()
+    return render_template('all_movies.html', results=data)
+
+# Fetch all genres for the dropdown
+@app.route('/movies/select-genre', methods=['GET', 'POST'])
+def select_genre():
+    genres = dbCode.get_all_genres()
+    selected_genre = None
+    movies = []
+
+    if request.method == 'POST':
+        selected_genre = request.form.get('genre')
+        movies = dbCode.get_top_movies_by_genre(selected_genre)
+
+    return render_template(
+        'select_genre.html',
+        genres=genres,
+        movies=movies,
+        selected_genre=int(selected_genre) if selected_genre else None
+    )
+
+
+# these two lines of code should always be the last in the file
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+#page 181 for db testing file - for dbCode?
